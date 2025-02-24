108ed2e2195e72e848c13cf64a2f934ffb749b8d
chkarthik17 <167787488+chkarthik17@users.noreply.github.com>
2025-02-24 22:49:24 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+let userInput = document.getElementById("defuser");
+let timerE1 = document.getElementById("timer");
+let startTime = 10;
+let timerId = setInterval(function() {
+    startTime -= 1;
+    timerE1.textContent = startTime;
+    if (startTime === 0) {
+        timerE1.textContent = 'BOOM';
+        clearInterval(timerId);
+    }
+}, 1000);
+
+userInput.addEventListener("keydown", function(event) {
+    let userValue = userInput.value;
+    if (event.key === "Enter" && userValue === "defuser" && startTime !== 0) {
+        timerE1.textContent = "YOU DID IT.";
+        userInput.value = "";
+    }
+})
